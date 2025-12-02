c686e6aa28ae6539c54c637bcd77d84ab0376215
Kaloyan Bozhkov <your-email@example.com>
2025-12-02 23:44:17 +0700

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,9 @@ import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig(
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
   eslint.configs.recommended,
   tseslint.configs.recommended,
 );
